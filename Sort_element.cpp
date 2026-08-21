d893c4c37af339281ba32bc2751b9969071da831
Dadakhalandar S A <dadugkt856775@gmail.com>
2026-08-21 18:03:08 +0530

    Add sorting functionality in Sort_element.cpp

---
+++ Sort_element.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> v = {40, 10, 30, 20, 50};
+
+    sort(v.begin(), v.end());
+
+    for (int x : v)
+        cout << x << " ";
+
+    return 0;
+}
