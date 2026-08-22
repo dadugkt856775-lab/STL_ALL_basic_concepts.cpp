ad871f85d454737f7f181a8294b14c539eda50bf
Dadakhalandar S A <dadugkt856775@gmail.com>
2026-08-22 18:42:01 +0530

    Add R_array.cpp to reverse an integer array
    Implement a program to reverse and print an array.

---
+++ R_array.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    vector<int> v = {10, 20, 30, 40, 50};
+
+    reverse(v.begin(), v.end());
+
+    for (int x : v)
+        cout << x << " ";
+
+    return 0;
+}
