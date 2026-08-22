7a9d18c2d9cb3f72c78bfef11a9102aa72df5722
Dadakhalandar S A <dadugkt856775@gmail.com>
2026-08-22 18:43:57 +0530

    Add Insert_ends.cpp to demonstrate deque usage
    Implement a simple program using deque to demonstrate insertion at both ends.

---
+++ Insert_ends.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <deque>
+using namespace std;
+
+int main() {
+    deque<int> d;
+
+    d.push_back(20);
+    d.push_back(30);
+    d.push_front(10);
+    d.push_front(5);
+
+    for (int x : d)
+        cout << x << " ";
+
+    return 0;
+}
